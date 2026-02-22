729b5084663adafb6a33e37622c2c16012948fbd
Sahil Chavan <sahilchavanajara327@gmail.com>
2026-02-22 12:49:27 +0530

    Fix: remove invalid PDO::ATTR_SSL_KEY constant causing Fatal Error on Render

--- config/database.php
+++ config/database.php
@@ -29,7 +29,6 @@ try {
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_TIMEOUT            => 10,
-        PDO::ATTR_SSL_KEY            => null,
     ]);
 } catch (PDOException $e) {
     http_response_code(500);
